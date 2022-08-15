d279544e7622a9503be89b04f1a3e55bba781ec6
TenDRILLL <sofyeer@gmail.com>
2022-08-15 10:24:41 +0300

    Init the twitchModule upon Discord ready.

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,10 +1,7 @@
 import Event from "../classes/Event";
-import {
-    Client
-} from "discord.js";
-import {
-    loadCommands
-} from "../automation/commandModule";
+import { Client } from "discord.js";
+import { loadCommands } from "../automation/commandModule";
+import { init } from "../automation/twitchModule";
 
 export class Ready extends Event{
     constructor() {
@@ -14,5 +11,6 @@ export class Ready extends Event{
     exec(bot: Client){
         console.log(`Connection to Discord Gateway formed, using account ${bot.user?.tag}.`);
         loadCommands();
+        init();
     }
 }
